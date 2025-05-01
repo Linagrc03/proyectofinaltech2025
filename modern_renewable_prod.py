d3bd4164a2cdc3dc824dd87d765295fac93eaf26
Lina García <146938627+Linagrc03@users.noreply.github.com>
2025-05-01 13:34:41 -0500

    Update modern_renewable_prod.py

--- modern_renewable_prod.py
+++ modern_renewable_prod.py
@@ -5,6 +5,8 @@ import plotly.express as px
 
 #carga de datos
 df = pd.read_csv("modern_renewable_prod.csv", sep=';')
+#imprimir información de la base de datos original
+print(len(df))
 
 #filtrar los paises suramericanos
 paises_suramericanos = [
